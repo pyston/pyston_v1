da993dfbc159065b8a5b32a71a082db8649d3a7c
Krzysztof Klinikowski <krzysztof.klinikowski@stxnext.pl>
2014-06-09 20:05:22 +0200

    More tests

--- test/tests/class_docstring.py
+++ test/tests/class_docstring.py
@@ -6,3 +6,14 @@ class TestResult(object):
 
 tr = TestResult()
 assert tr.__doc__ == '\n    OMG PONIES\n    '
+
+class TestResult2(object):
+    """
+    OMG PONIES 2
+    """
+    def __init__(self):
+        """ THIS SHOULD BE IGNORED """
+        pass
+
+tr = TestResult2()
+assert tr.__doc__ == '\n    OMG PONIES 2\n    '
