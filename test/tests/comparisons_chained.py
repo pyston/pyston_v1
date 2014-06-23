7cb5ed429ec984c0cf707b75d5b340e8f7b374be
Kevin Modzelewski <kmod@dropbox.com>
2014-06-23 15:17:55 -0700

    Update comparisons_chained.py

--- test/tests/comparisons_chained.py
+++ test/tests/comparisons_chained.py
@@ -1,6 +1,20 @@
+# expected: fail
+
+class C(object):
+    def __init__(self, n):
+        self.n = n
+
+    def __lt__(self, rhs):
+        print "lt"
+        return self.n < rhs.n
+
+    def __le__(self, rhs):
+        print "le"
+        return self.n <= rhs.n
+
 def f(n):
     print "f(%d)" % n
-    return n
+    return C(n)
 
 # f(3) shouldn't get called:
 f(1) <= f(2) < f(1) < f(3)
