2b1ced5560de71fd86a138e34f8441438a6b6120
Vinzenz Feenstra <evilissimo@gmail.com>
2014-07-22 18:48:32 +0200

    More integration and AST_Ellipsis type added
    Signed-off-by: Vinzenz Feenstra <evilissimo@gmail.com>

--- src/codegen/pypa-parser.cpp
+++ src/codegen/pypa-parser.cpp
@@ -30,8 +30,15 @@
 
 namespace pyston {
 
+void location(AST* t, pypa::Ast& a) {
+    t->lineno = a.line;
+    t->col_offset = a.column;
+}
+
 AST_expr* readItem(pypa::AstExpression& e);
 AST_stmt* readItem(pypa::AstStatement& s);
+AST_ExceptHandler* readItem(pypa::AstExcept&);
+AST_ExceptHandler* readItem(pypa::AstExceptPtr);
 
 template <typename T> auto readItem(std::shared_ptr<T>& t) -> decltype(readItem(*t)) {
     if (t)
@@ -120,12 +127,15 @@ std::string readName(pypa::AstExpression& e) {
 }
 
 std::string readName(pypa::AstExpr& n) {
-    assert(n);
-    return readName(*n);
+    if(!n) {
+        return std::string();
+    }
+    return std::move(readName(*n));
 }
 
 AST_keyword* readItem(pypa::AstKeyword& k) {
     AST_keyword* ptr = new AST_keyword();
+    location(ptr, k);
     ptr->arg = readName(k.name);
     ptr->value = readItem(k.value);
     return ptr;
@@ -140,15 +150,19 @@ void readVector(std::vector<AST_keyword*>& t, pypa::AstExprList& items) {
 
 template <typename T, typename U> void readVector(std::vector<T*>& t, std::vector<U>& u) {
     for (auto& item : u) {
-        assert(item);
-        t.push_back(readItem(*item));
+        if(!item) {
+            t.push_back(nullptr);
+        }
+        else {
+            t.push_back(readItem(*item));
+        }
     }
 }
 
 void readVector(std::vector<AST_expr*>& t, pypa::AstExpression& u) {
-    if (u.type == pypa::AstType::Expressions) {
-        pypa::AstExpressions& e = static_cast<pypa::AstExpressions&>(u);
-        for (auto& item : e.items) {
+    if (u.type == pypa::AstType::Tuple) {
+        pypa::AstTuple& e = static_cast<pypa::AstTuple&>(u);
+        for (auto& item : e.elements) {
             assert(item);
             t.push_back(readItem(*item));
         }
@@ -169,9 +183,59 @@ void readVector(std::vector<AST_stmt*>& t, pypa::AstStatement& u) {
     }
 }
 
-void location(AST* t, pypa::Ast& a) {
-    t->lineno = a.line;
-    t->col_offset = a.column;
+AST_comprehension* readItem(pypa::AstComprehension & c) {
+    AST_comprehension* ptr = new AST_comprehension();
+    ptr->target = readItem(c.target);
+    ptr->iter = readItem(c.iter);
+    readVector(ptr->ifs, c.ifs);
+    return ptr;
+}
+
+AST_comprehension* readItem(pypa::AstComprPtr c) {
+    if(c) return readItem(*c);
+    return nullptr;
+}
+
+void readVector(std::vector<AST_comprehension*>& t, pypa::AstExprList& u) {
+    for(auto & e : u) {
+        assert(e && e->type == pypa::AstType::Comprehension);
+        t.push_back(readItem(static_cast<pypa::AstComprehension&>(*e)));
+    }
+}
+
+void readVector(std::vector<AST_stmt*>& t, pypa::AstStmt u) {
+    if(u) {
+        readVector(t, *u);
+    }
+}
+
+AST_ExceptHandler* readItem(pypa::AstExcept & e) {
+    AST_ExceptHandler* ptr = new AST_ExceptHandler();
+    location(ptr, e);
+    readVector(ptr->body, e.body);
+    ptr->name = readItem(e.name);
+    ptr->type = readItem(e.type);
+    return ptr;
+}
+
+AST_ExceptHandler* readItem(pypa::AstExceptPtr ptr) {
+    assert(ptr);
+    return readItem(*ptr);
+}
+
+AST_alias* readItem(pypa::AstAlias & a) {
+    return new AST_alias(readName(a.name), readName(a.as_name));
+}
+
+AST_arguments* readItem(pypa::AstArguments & a) {
+    AST_arguments * ptr = new AST_arguments();
+    location(ptr, a);
+    readVector(ptr->defaults, a.defaults);
+    ptr->defaults.erase(std::remove(ptr->defaults.begin(), ptr->defaults.end(), nullptr), ptr->defaults.end());
+    readVector(ptr->args, a.arguments);
+    ptr->kwarg = std::move(readName(a.kwargs));
+    ptr->vararg = std::move(readName(a.args));
+    return ptr;
 }
 
 struct expr_dispatcher {
@@ -193,6 +257,15 @@ struct expr_dispatcher {
         return nullptr;
     }
 
+    ResultPtr read(pypa::AstAttribute& a) {
+        AST_Attribute* ptr = new AST_Attribute();
+        location(ptr, a);
+        ptr->value = readItem(a.value);
+        ptr->attr = std::move(readName(a.attribute));
+        ptr->ctx_type = readItem(a.context);
+        return ptr;
+    }
+
     ResultPtr read(pypa::AstBoolOp& b) {
         AST_BoolOp* ptr = new AST_BoolOp();
         location(ptr, b);
@@ -210,16 +283,20 @@ struct expr_dispatcher {
         return ptr;
     }
 
-    ResultPtr read(pypa::AstUnaryOp & b) {
-        AST_UnaryOp* ptr = new AST_UnaryOp();
-        location(ptr, b);
-        ptr->op_type = readItem(b.op);
-        ptr->operand = readItem(b.operand);
+    ResultPtr read(pypa::AstCall& c) {
+        AST_Call* ptr = new AST_Call();
+        location(ptr, c);
+        readVector(ptr->args, c.arglist.arguments);
+        readVector(ptr->keywords, c.arglist.keywords);
+        ptr->func = readItem(c.function);
+        ptr->starargs = readItem(c.arglist.args);
+        ptr->kwargs = readItem(c.arglist.kwargs);
         return ptr;
     }
 
     ResultPtr read(pypa::AstCompare & c) {
         AST_Compare * ptr = new AST_Compare();
+        location(ptr, c);
         ptr->left = readItem(c.left) ;
         pypa::AstExpr r = c.right;
         ptr->ops.push_back(readItem(c.op));
@@ -233,54 +310,157 @@ struct expr_dispatcher {
         return ptr;
     }
 
-
-    ResultPtr read(pypa::AstExpressions& a) {
-        assert(a.items.empty() || a.items.size() == 1);
-        if (!a.items.empty()) {
-            return readItem(a.items.front());
-        }
+    ResultPtr read(pypa::AstComprehension & c) {
+        assert("This should not be called" && false);
         return nullptr;
     }
 
-    ResultPtr read(pypa::AstStr& s) {
-        AST_Str* ptr = new AST_Str();
-        ptr->s = s.value;
+    ResultPtr read(pypa::AstDict& d) {
+        AST_Dict* ptr = new AST_Dict();
+        location(ptr, d);
+        readVector(ptr->keys, d.keys);
+        readVector(ptr->values, d.values);
+        return ptr;
+    }
+
+    ResultPtr read(pypa::AstDictComp& d) {
+        AST_DictComp* ptr = new AST_DictComp();
+        location(ptr, d);
+        ptr->key = readItem(d.key);
+        ptr->value = readItem(d.value);
+        readVector(ptr->generators, d.generators);
+        return ptr;
+    }
+
+    ResultPtr read(pypa::AstEllipsis & e) {
+        AST_Ellipsis * ptr = new AST_Ellipsis();
+        location(ptr, e);
+        return ptr;
+    }
+
+    ResultPtr read(pypa::AstExtSlice & e) {
+        AST_ExtSlice * ptr = new AST_ExtSlice();
+        location(ptr, e);
+        readVector(ptr->dims, e.dims);
+        return ptr;
+    }
+
+    ResultPtr read(pypa::AstIfExpr & i) {
+        AST_IfExp* ptr = new AST_IfExp();
+        location(ptr, i);
+        ptr->body = readItem(i.body);
+        ptr->test = readItem(i.test);
+        ptr->orelse = readItem(i.orelse);
+        return ptr;
+    }
+
+    ResultPtr read(pypa::AstIndex & i) {
+        AST_Index* ptr = new AST_Index();
+        location(ptr, i);
+        ptr->value = readItem(i.value);
+        return ptr;
+    }
+
+    ResultPtr read(pypa::AstList & l) {
+        AST_List* ptr = new AST_List();
+        location(ptr, l);
+        readVector(ptr->elts, l.elements);
+        ptr->ctx_type = readItem(l.context);
+        return ptr;
+    }
+
+    ResultPtr read(pypa::AstListComp & l) {
+        AST_ListComp* ptr = new AST_ListComp();
+        location(ptr, l);
+        readVector(ptr->generators, l.generators);
+        ptr->elt = readItem(l.element);
         return ptr;
     }
 
     ResultPtr read(pypa::AstName& a) {
         AST_Name* ptr = new AST_Name();
+        location(ptr, a);
         ptr->ctx_type = readItem(a.context);
         ptr->id = a.id;
         return ptr;
     }
 
-    ResultPtr read(pypa::AstAttribute& a) {
-        AST_Attribute* ptr = new AST_Attribute();
-        ptr->value = readItem(a.value);
-        ptr->attr = readName(a.attribute);
-        ptr->ctx_type = readItem(a.context);
-        return ptr;
-    }
-
-    ResultPtr read(pypa::AstCall& c) {
-        AST_Call* ptr = new AST_Call();
-        location(ptr, c);
-        readVector(ptr->args, c.arglist.arguments);
-        readVector(ptr->keywords, c.arglist.keywords);
-        ptr->func = readItem(c.function);
-        ptr->starargs = readItem(c.arglist.args);
-        ptr->kwargs = readItem(c.arglist.kwargs);
+    ResultPtr read(pypa::AstNone& n) {
+        AST_Name* ptr = new AST_Name();
+        location(ptr, n);
+        ptr->ctx_type = AST_TYPE::Load;
+        ptr->id = "None";
         return ptr;
     }
 
     ResultPtr read(pypa::AstNumber& c) {
         AST_Num* ptr = new AST_Num();
+        location(ptr, c);
         ptr->num_type = c.num_type == pypa::AstNumber::Float ? AST_Num::FLOAT : AST_Num::INT;
         if(ptr->num_type == AST_Num::INT)
             ptr->n_int = c.integer;
         else
             ptr->n_float = c.floating;
+        return ptr;
+    }
+
+    ResultPtr read(pypa::AstRepr &r) {
+        AST_Repr* ptr = new AST_Repr();
+        location(ptr, r);
+        ptr->value = readItem(r.value);
+        return ptr;
+    }
+
+    ResultPtr read(pypa::AstSet & s) {
+        /*
+        AST_Set* ptr = new AST_Set();
+        location(ptr, s);
+        readVector(ptr->elts, s.elements);
+        ptr->ctx_type = readItem(s.context);
+        return ptr;
+        */
+        assert("Set not implemented" && false);
+        return nullptr;
+    }
+
+    ResultPtr read(pypa::AstSlice & s) {
+        AST_Slice * ptr = new AST_Slice();
+        location(ptr, s);
+        ptr->lower = readItem(s.lower);
+        ptr->upper = readItem(s.upper);
+        ptr->step = readItem(s.step);
+        return ptr;
+    }
+
+    ResultPtr read(pypa::AstStr& s) {
+        AST_Str* ptr = new AST_Str();
+        location(ptr, s);
+        ptr->s = s.value;
+        return ptr;
+    }
+
+    ResultPtr read(pypa::AstSubscript & s) {
+        AST_Subscript * ptr = new AST_Subscript();
+        location(ptr, s);
+        ptr->value = readItem(s.value);
+        ptr->ctx_type = readItem(s.context);
+        ptr->slice = readItem(s.slice);
+        return ptr;
+    }
+
+    ResultPtr read(pypa::AstTuple & t) {
+        AST_Tuple* ptr = new AST_Tuple();
+        location(ptr, t);
+        readVector(ptr->elts, t.elements);
+        ptr->ctx_type = readItem(t.context);
+        return ptr;
+    }
+
+    ResultPtr read(pypa::AstUnaryOp & b) {
+        AST_UnaryOp* ptr = new AST_UnaryOp();
+        location(ptr, b);
+        ptr->op_type = readItem(b.op);
+        ptr->operand = readItem(b.operand);
         return ptr;
     }
 };
@@ -307,7 +487,7 @@ struct stmt_dispatcher {
     ResultPtr read(pypa::AstAssign& a) {
         AST_Assign* ptr = new AST_Assign();
         location(ptr, a);
-        readVector(ptr->targets, *a.targets);
+        readVector(ptr->targets, a.targets);
         ptr->value = readItem(a.value);
         return ptr;
     }
@@ -320,34 +500,6 @@ struct stmt_dispatcher {
         return ptr;
     }
 
-    ResultPtr read(pypa::AstBreak& b) {
-        AST_Break* ptr = new AST_Break();
-        location(ptr, b);
-        return ptr;
-    }
-
-    ResultPtr read(pypa::AstFor & f) {
-        AST_For * ptr = new AST_For();
-        ptr->target = readItem(f.target);
-        ptr->iter = readItem(f.iter);
-        if(f.body) readVector(ptr->body, *f.body);
-        if(f.orelse) readVector(ptr->orelse, *f.orelse);
-        return ptr;
-    }
-
-    ResultPtr read(pypa::AstWith& w) {
-        AST_With* ptr = new AST_With();
-        location(ptr, w);
-        ptr->optional_vars = readItem(w.optional);
-        ptr->context_expr = readItem(w.context);
-        if (w.body->type != pypa::AstType::Suite) {
-            ptr->body.push_back(readItem(w.body));
-        } else {
-            readVector(ptr->body, static_cast<pypa::AstSuite&>(*w.body).items);
-        }
-        return ptr;
-    }
-
     ResultPtr read(pypa::AstAugAssign & a) {
         AST_AugAssign * ptr = new AST_AugAssign();
         location(ptr, a);
@@ -357,12 +509,183 @@ struct stmt_dispatcher {
         return ptr;
     }
 
+    ResultPtr read(pypa::AstBreak& b) {
+        AST_Break* ptr = new AST_Break();
+        location(ptr, b);
+        return ptr;
+    }
+
+    ResultPtr read(pypa::AstClassDef & c) {
+        AST_ClassDef* ptr = new AST_ClassDef();
+        location(ptr, c);
+        if(c.bases) readVector(ptr->bases, *c.bases);
+        readVector(ptr->decorator_list, c.decorators);
+        readVector(ptr->body, c.body);
+        ptr->name = std::move(readName(c.name));
+        return ptr;
+    }
+
+    ResultPtr read(pypa::AstContinue & c) {
+        AST_Continue* ptr = new AST_Continue();
+        location(ptr, c);
+        return ptr;
+    }
+
+    ResultPtr read(pypa::AstDelete& d) {
+        AST_Delete * ptr = new AST_Delete();
+        location(ptr, d);
+        readVector(ptr->targets, *d.targets);
+        return ptr;
+    }
+
+    ResultPtr read(pypa::AstExpressionStatement& e) {
+        AST_Expr * ptr = new AST_Expr();
+        location(ptr, e);
+        ptr->value = readItem(e.expr);
+        return ptr;
+    }
+
+    ResultPtr read(pypa::AstFor & f) {
+        AST_For * ptr = new AST_For();
+        location(ptr, f);
+        ptr->target = readItem(f.target);
+        if(f.iter) ptr->iter = readItem(f.iter);
+        if(f.body) readVector(ptr->body, *f.body);
+        if(f.orelse) readVector(ptr->orelse, *f.orelse);
+        return ptr;
+    }
+
+    ResultPtr read(pypa::AstFunctionDef& f) {
+        AST_FunctionDef * ptr = new AST_FunctionDef();
+        location(ptr, f);
+        readVector(ptr->decorator_list, f.decorators);
+        ptr->name = std::move(readName(f.name));
+        ptr->args = readItem(f.args);
+        readVector(ptr->body, f.body);
+        return ptr;
+    }
+
+    ResultPtr read(pypa::AstGlobal & g) {
+        AST_Global* ptr = new AST_Global();
+        location(ptr, g);
+        ptr->names.resize(g.names.size());
+        for (size_t i = 0; i < g.names.size(); ++i) {
+            ptr->names[i] = std::move(readName(*g.names[i]));
+        }
+        return ptr;
+    }
+
+    ResultPtr read(pypa::AstIf& i) {
+        AST_If* ptr = new AST_If();
+        location(ptr, i);
+        readVector(ptr->body, i.body);
+        ptr->test = readItem(i.test);
+        assert(ptr->test != 0);
+        readVector(ptr->orelse, i.orelse);
+        return ptr;
+    }
+
+    ResultPtr read(pypa::AstImport& i) {
+        AST_Import* ptr = new AST_Import();
+        location(ptr, i);
+        if(i.names->type == pypa::AstType::Tuple) {
+            for(auto & name : static_cast<pypa::AstTuple&>(*i.names).elements) {
+                assert(name->type == pypa::AstType::Alias);
+                ptr->names.push_back(readItem(static_cast<pypa::AstAlias&>(*name)));
+            }
+        }
+        else {
+            assert(i.names->type == pypa::AstType::Alias);
+            ptr->names.push_back(readItem(static_cast<pypa::AstAlias&>(*i.names)));
+        }
+        return ptr;
+    }
+
+    ResultPtr read(pypa::AstImportFrom& i) {
+        AST_ImportFrom* ptr = new AST_ImportFrom();
+        location(ptr, i);
+        ptr->module = std::move(readName(i.module));
+        if(i.names->type == pypa::AstType::Tuple) {
+            for(auto & name : static_cast<pypa::AstTuple&>(*i.names).elements) {
+                assert(name->type == pypa::AstType::Alias);
+                ptr->names.push_back(readItem(static_cast<pypa::AstAlias&>(*name)));
+            }
+        }
+        else {
+            assert(i.names->type == pypa::AstType::Alias);
+            ptr->names.push_back(readItem(static_cast<pypa::AstAlias&>(*i.names)));
+        }
+        ptr->level = i.level;
+        return ptr;
+    }
+
+    ResultPtr read(pypa::AstPass & p) {
+        AST_Pass* ptr = new AST_Pass();
+        location(ptr, p);
+        return ptr;
+    }
+
     ResultPtr read(pypa::AstPrint& p) {
         AST_Print* ptr = new AST_Print();
         location(ptr, p);
         ptr->dest = readItem(p.destination);
         ptr->nl = p.newline;
         readVector(ptr->values, p.values);
+        return ptr;
+    }
+
+    ResultPtr read(pypa::AstRaise &r) {
+        AST_Raise* ptr = new AST_Raise();
+        location(ptr, r);
+        ptr->arg0 = readItem(r.arg0);
+        ptr->arg1 = readItem(r.arg1);
+        ptr->arg2 = readItem(r.arg2);
+        return ptr;
+    }
+
+    ResultPtr read(pypa::AstSuite & s) {
+        return nullptr;
+    }
+
+    ResultPtr read(pypa::AstReturn &r) {
+        AST_Return* ptr = new AST_Return();
+        location(ptr, r);
+        ptr->value = readItem(r.value);
+        return ptr;
+    }
+
+    ResultPtr read(pypa::AstTryExcept& t) {
+        AST_TryExcept* ptr = new AST_TryExcept();
+        location(ptr, t);
+        readVector(ptr->body, t.body);
+        readVector(ptr->orelse, t.orelse);
+        readVector(ptr->handlers, t.handlers);
+        return ptr;
+    }
+
+    ResultPtr read(pypa::AstTryFinally& t) {
+        AST_TryFinally* ptr = new AST_TryFinally();
+        location(ptr, t);
+        readVector(ptr->body, t.body);
+        readVector(ptr->finalbody, t.final_body);
+        return ptr;
+    }
+
+    ResultPtr read(pypa::AstWith& w) {
+        AST_With* ptr = new AST_With();
+        location(ptr, w);
+        ptr->optional_vars = readItem(w.optional);
+        ptr->context_expr = readItem(w.context);
+        readVector(ptr->body, w.body);
+        return ptr;
+    }
+
+    ResultPtr read(pypa::AstWhile& w) {
+        AST_While* ptr = new AST_While();
+        location(ptr, w);
+        ptr->test = readItem(w.test);
+        readVector(ptr->body, w.body);
+        readVector(ptr->orelse, w.orelse);
         return ptr;
     }
 };
