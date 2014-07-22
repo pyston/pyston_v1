5a8baaf7190ad9661a34c735cc2b6e5f29400f97
Travis Hance <tjhance@dropbox.coM>
2014-07-22 09:01:03 +0000

    small fix to test script

--- tools/tester.py
+++ tools/tester.py
@@ -73,7 +73,6 @@ def get_expected_output(fn):
     code = p.wait()
 
     r = code, out, err
-    assert code >= 0, "CPython exited with an unexpected exit code: %d" % (code,)
 
     cPickle.dump(r, open(cache_fn, 'w'))
     return r
@@ -150,7 +149,10 @@ def run_test(fn, check_stats, run_memcheck):
             stats[k.strip()] = int(v)
 
     expected_code, expected_out, expected_err = get_expected_output(fn)
-    if code != expected_code:
+    if expected_code < 0:
+        r += "    CPython gave unexpected exit code: %d" % expected_code
+        return r
+    elif code != expected_code:
         color = 31 # red
 
         if code == 0:
