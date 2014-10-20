4f9808054de22d95c8da0732cdfa2195733ab5e3
Ryan Pearl <rpearl@dropbox.com>
2014-10-20 15:54:38 -0700

    Make check_format actually useful

--- tools/check_format.sh
+++ tools/check_format.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/usr/bin/env bash
 set -eu
 failed=0
 for fn in $(find . -name '*.cpp' -o -name '*.h'); do
-    $1 -style=file -output-replacements-xml $fn | grep -q "replacement offset" && { echo $fn "failed clang-format check"; failed=1; }
+    diff -u $fn <($1 -style=file $fn) || failed=1
 done
 
 if [ $failed -eq 0 ]; then
