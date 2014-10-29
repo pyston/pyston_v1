pyston/pyston_v1

commit cfe83db05f85936fe92656b0e5ab2ec32929f6ef

Fix IOError format string when opening non-existent file

src/runtime/builtin_modules/builtins.cpp
@@ -211,7 +211,7 @@ Box* open(Box* arg1, Box* arg2) {
 
     FILE* f = fopen(fn.c_str(), mode.c_str());
     if (!f)
-        raiseExcHelper(IOError, "%s: '%s' '%s'", strerror(errno), fn.c_str());
+        raiseExcHelper(IOError, "[Error %d] %s: '%s'", errno, strerror(errno), fn.c_str());
 
     return new BoxedFile(f, fn, mode);
 }

test/tests/file.py
@@ -39,3 +39,8 @@ with open('../README.md') as f:
     print lines[:5]
     print lines[-5:]
 
+# Check that opening a non-existent file results in an IOError.
+try:
+   f = open('this-should-definitely-not-exist.txt')
+except IOError as e:
+   print str(e)

commit 6ff77a6a142b1e783e86276e40f8f4caf1b43ec7

Fix typo in IOError format string
This fixes an annoying typo introduced in cfe83db.

src/runtime/builtin_modules/builtins.cpp
@@ -211,7 +211,7 @@ Box* open(Box* arg1, Box* arg2) {
 
     FILE* f = fopen(fn.c_str(), mode.c_str());
     if (!f)
-        raiseExcHelper(IOError, "[Error %d] %s: '%s'", errno, strerror(errno), fn.c_str());
+        raiseExcHelper(IOError, "[Errno %d] %s: '%s'", errno, strerror(errno), fn.c_str());
 
     return new BoxedFile(f, fn, mode);
 }