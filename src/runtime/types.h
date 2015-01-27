9ec28ecd5df51ea8a02ea6f581389e7c9d40778e
Marius Wachtler <undingen@gmail.com>
2015-01-28 00:33:12 +0100

    Mark enumerate_cls as safe for type call rewriting Reduces slowpath_typecall from 40000 to 30 for spectral_norm

--- src/runtime/types.h
+++ src/runtime/types.h
@@ -78,9 +78,9 @@ extern "C" Box* getSysStdout();
 
 extern "C" {
 extern BoxedClass* object_cls, *type_cls, *bool_cls, *int_cls, *long_cls, *float_cls, *str_cls, *function_cls,
-    *none_cls, *instancemethod_cls, *list_cls, *slice_cls, *module_cls, *dict_cls, *tuple_cls, *file_cls, *xrange_cls,
-    *member_cls, *method_cls, *closure_cls, *generator_cls, *complex_cls, *basestring_cls, *unicode_cls, *property_cls,
-    *staticmethod_cls, *classmethod_cls, *attrwrapper_cls;
+    *none_cls, *instancemethod_cls, *list_cls, *slice_cls, *module_cls, *dict_cls, *tuple_cls, *file_cls,
+    *enumerate_cls, *xrange_cls, *member_cls, *method_cls, *closure_cls, *generator_cls, *complex_cls, *basestring_cls,
+    *unicode_cls, *property_cls, *staticmethod_cls, *classmethod_cls, *attrwrapper_cls;
 }
 extern "C" {
 extern Box* None, *NotImplemented, *True, *False;
