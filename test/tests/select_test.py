b1a1a17151fd0b898554848c0398770f3db14633
Kevin Modzelewski <kmod@dropbox.com>
2015-01-28 00:57:38 -0800

    Deal with the fact that EPOLLET can be negative in CPython
    My theory is that this is because it overflows a signed int in
    32-bit builds.  This should hopefully fix #272

--- test/tests/select_test.py
+++ test/tests/select_test.py
@@ -3,5 +3,18 @@ import select
 for k in sorted(dir(select)):
     if not k.startswith("EPOLL") and not k.startswith("POLL"):
         continue
-    print k, getattr(select, k)
+
+    if k == "EPOLLET":
+        # On 32-bit versions of CPython, select.EPOLLET (==1<<31) overflows a 32-bit signed integer
+        # and becomes INT_MIN.  But really, it's a bitmask so it's an unsigned value and imho should
+        # be provided as a positive number.
+        # Since we only build Pyston in 64-bit mode, EPOLLET is always positive in Pyston.  If we
+        # see that it's negative, just make sure that it probably hit this behavior (wrapped around)
+        v = select.EPOLLET
+        if v < 0:
+            import sys
+            assert (-v) > sys.maxint
+        print abs(v)
+    else:
+        print k, getattr(select, k)
 
