d3ba142d3920b5354d262af0ef33fc8860aef464
Kevin Modzelewski <kmod@dropbox.com>
2015-01-29 02:42:43 -0800

    Identify + fix codegen bug
    In certain cases we wouldn't do well if we were sure that a type error
    would occur (ex indexing into what we know is None) -- we would error in
    codegen instead of generating the code to throw the error at runtime.
    
    (sneak in another travis.yml attempt)

---
+++ test/tests/type_errors.py
@@ -0,0 +1,34 @@
+# Regression test: make sure that we can handle various kinds of written type errors.
+# ex if we can prove that an expression would throw an error due to type issues, we should
+# still compile and run the function just fine.
+#
+# We definitely try to support this, but it can be tricky sometimes since the different phases
+# (mainly, type analysis and irgen) have to agree on the way they deal with the error.  If they
+# don't, irgen will crash after it detects a mismatch with type analysis.
+
+def f(p):
+    if not "opaque branch condition":
+        k = p[1]
+        print k
+
+        l = len(p)
+        print l
+
+        t = p + 1
+        print t
+
+# Run in a loop to trigger OSR:
+for i in xrange(20):
+    f(None)
+print "done"
+
+
+try:
+    print 1.0 & 1.0
+except TypeError, e:
+    print e
+
+try:
+    print 1.0[0]
+except TypeError, e:
+    print e
