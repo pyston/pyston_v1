b921c01f3c7d8d3c0c55868bae1ee4abfc62ca3e
Chris Toshok <toshok@dropbox.com>
2015-02-13 22:05:18 +0000

    add a test.  verified that it fails (with my current ulimit) without the destructor.  succeeds with

---
+++ test/tests/file-destructor.py
@@ -0,0 +1,13 @@
+try:
+  import gc
+  have_gc = True
+except e:
+  pass
+
+def open_lots_of_files():
+  for x in range(0, 10000):
+    f = open("/dev/null")
+    if have_gc:
+      gc.collect();
+
+open_lots_of_files()
