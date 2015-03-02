df53aebfead778083a93f1757147256444f3bf3f
Michael Arntzenius <daekharel@gmail.com>
2015-03-02 14:17:00 -0800

    add test/tests/dash_c.py

---
+++ test/tests/dash_c.py
@@ -0,0 +1,18 @@
+import sys
+import subprocess
+
+me = sys.executable
+
+with open('/dev/null')as ignore:
+    # We don't (yet?) require exact stderr or return code compatibility w/
+    # python. So we just check that we succeed or fail as appropriate.
+    def run(args):
+        print 0 == subprocess.call([me] + args, stderr=ignore)
+
+    run(["-c", "print 2 + 2"])
+    run(["-c", "import sys; print sys.argv", "hello", "world"])
+    run(["-c", "import sys; print sys.argv", "-c", "this is ignored"])
+
+    run(["-c"])
+    run(["-c", "-c"])
+    run(["-c", "this should not work"])
