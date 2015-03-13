74ac1a3cfa562b010143619ea01fee7d8405583d
Kevin Modzelewski <kmod@dropbox.com>
2015-03-12 20:56:55 -0700

    datetime.strftime()

--- test/tests/datetime_test.py
+++ test/tests/datetime_test.py
@@ -9,3 +9,5 @@ print str(datetime.timedelta(0))
 
 # now() works on both the class and on its instances:
 datetime.datetime.now().now()
+
+print datetime.datetime(1924, 2, 3).strftime("%B %d, %Y - %X")
