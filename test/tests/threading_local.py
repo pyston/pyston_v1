7212a261fe063d09c61b20dc0309f68fe2259d35
Kevin Modzelewski <kmod@dropbox.com>
2015-03-18 00:58:06 -0700

    Add some notes about threading.locals
    Marius beat me to the punch with the fix this time :)

--- test/tests/threading_local.py
+++ test/tests/threading_local.py
@@ -1,3 +1,4 @@
+import gc
 import threading
 
 a = threading.local()
@@ -7,9 +8,14 @@ def f():
     a.x = "goodbye world"
     print a.x
 
-thread = threading.Thread(target=f)
-thread.start()
-thread.join()
+def test():
+    thread = threading.Thread(target=f)
+    thread.start()
+    thread.join()
 
-print a.x
-print getattr(a, "doesnt_exist", 5)
+    print a.x
+    print getattr(a, "doesnt_exist", 5)
+
+for i in xrange(10):
+    test()
+    gc.collect()
