b0ba8e15d931f7bed61466708f184d60744a8f60
Marius Wachtler <undingen@gmail.com>
2015-03-24 22:45:40 +0100

    Add missing boxGCHandler() calls

--- src/runtime/types.cpp
+++ src/runtime/types.cpp
@@ -465,6 +465,8 @@ static void typeSetDict(Box* obj, Box* val, void* context) {
 Box* dict_descr = NULL;
 
 extern "C" void instancemethodGCHandler(GCVisitor* v, Box* b) {
+    boxGCHandler(v, b);
+
     BoxedInstanceMethod* im = (BoxedInstanceMethod*)b;
 
     if (im->obj) {
@@ -474,6 +476,8 @@ extern "C" void instancemethodGCHandler(GCVisitor* v, Box* b) {
 }
 
 extern "C" void propertyGCHandler(GCVisitor* v, Box* b) {
+    boxGCHandler(v, b);
+
     BoxedProperty* prop = (BoxedProperty*)b;
 
     if (prop->prop_get)
@@ -487,6 +491,8 @@ extern "C" void propertyGCHandler(GCVisitor* v, Box* b) {
 }
 
 extern "C" void staticmethodGCHandler(GCVisitor* v, Box* b) {
+    boxGCHandler(v, b);
+
     BoxedStaticmethod* sm = (BoxedStaticmethod*)b;
 
     if (sm->sm_callable)
@@ -494,6 +500,8 @@ extern "C" void staticmethodGCHandler(GCVisitor* v, Box* b) {
 }
 
 extern "C" void classmethodGCHandler(GCVisitor* v, Box* b) {
+    boxGCHandler(v, b);
+
     BoxedClassmethod* cm = (BoxedClassmethod*)b;
 
     if (cm->cm_callable)
