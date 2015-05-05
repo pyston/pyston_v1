295e023312ed87559846ac859391b4690658e40e
Kevin Modzelewski <kmod@dropbox.com>
2015-05-05 01:13:28 -0700

    Make the import tests slightly more robust
    I guess in CPython, __file__ will be the path to the pyc if that's how
    the library was loaded.  (We don't do that but that difference isn't the
    issue here.)  So that makes these tests, which print __file__, sensitive
    to whether the last run of the test was through cpython (pyc exists and
    __file__ ends in pyc) or through pyston (pyc exists but was wasn't valid
    for cpython, and __file__ ends in py)
    
    Just do some canonicalization when printing

--- test/tests/test_package/absolute_import.py
+++ test/tests/test_package/absolute_import.py
@@ -1,4 +1,4 @@
 import os
 import import_target
 
-print 3, import_target.__name__, os.path.normpath(import_target.__file__)
+print 3, import_target.__name__, os.path.normpath(import_target.__file__).replace(".pyc", ".py")
