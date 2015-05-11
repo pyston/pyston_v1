358610b9f72ace751b9f921cd885a87f7741b872
Kevin Modzelewski <kmod@dropbox.com>
2015-05-11 16:21:41 -0700

    Try to run some sqlalchemy tests manually
    Using their test runner is quite hard since it tries to override a lot
    of basic functionality (ex for enhanced tracebacks).  So just do some
    manual test discovery, and only run the tests that cpython can pass
    using this setup.
    
    We still fail a couple more though.

---
+++ test/tests/no_newline.py
@@ -0,0 +1,4 @@
+# fail-if: '-x' not in EXTRA_JIT_ARGS
+class C(object):
+    """
+    """
---
+++ test/tests/str_adding.py
@@ -0,0 +1,24 @@
+# expected: fail
+
+# str has some weird adding behavior that cannot be replicated with Python code.
+# Usually, if an __add__ throws an error, then __radd__ isn't tried.
+# But string addition is defined in sq_concat, which is tried after __add__ and
+# __radd__.  And string addition will throw a TypeError.
+
+class C(object):
+    def __add__(self, rhs):
+        raise TypeError("dont support add")
+
+class D(object):
+    def __radd__(self, lhs):
+        return 3.14
+
+try:
+    print "".__add__(D())
+except TypeError as e:
+    print e
+try:
+    print C() + D()
+except TypeError as e:
+    print e
+print "" + D()
