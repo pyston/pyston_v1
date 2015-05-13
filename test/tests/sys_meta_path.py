9ebceb76496665d98f71f57e15bfb336a3092bd8
Kevin Modzelewski <kmod@dropbox.com>
2015-05-13 16:48:30 -0700

    Fix path_hooks issue
    Don't pass path to this version of find_module (unlike finders in meta_path).

--- test/tests/sys_meta_path.py
+++ test/tests/sys_meta_path.py
@@ -20,13 +20,40 @@ class Finder(object):
         print "find", fullname, path
         return Loader()
 
+class Finder2(object):
+    def __new__(cls, path):
+        print "new2", path
+        return object.__new__(cls)
+
+    def __init__(self, path):
+        self.path = path
+
+    def find_module(self, fullname):
+        print "find2", self.path, fullname
+        return None
+
+# Fill the importer cache, so that we don't have to worry about the exact
+# sys.path:
 try:
     import a.b.test
 except ImportError, e:
-    print "caught import error"
     # The error CPython gives here is "No module named a.b.test".  Both we and PyPy think this
     # is wrong and that the error should be "No module named a".
     # So unfortunately we can't print out the error message.
+    print "caught import error 1"
+
+sys.path_hooks.append(Finder2)
+try:
+    import a.b.test
+except ImportError, e:
+    print "caught import error 2"
+
+sys.path.append("/my_magic_directory")
+
+try:
+    import a.b.test
+except ImportError, e:
+    print "caught import error 3"
 
 sys.meta_path.append(Finder())
 import a
