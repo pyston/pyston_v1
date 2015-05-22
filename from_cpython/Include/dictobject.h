b59499c9c534bad1ab9e2fefc471652477e07b2c
Michael Arntzenius <daekharel@gmail.com>
2015-05-22 13:52:10 -0700

    add comments on code that needs to be changed for libgcc 4.9

--- from_cpython/Include/dictobject.h
+++ from_cpython/Include/dictobject.h
@@ -94,7 +94,8 @@ struct _dictobject {
 #endif
 typedef struct {
     PyObject_HEAD;
-    char _filler[48];
+    char _filler[48];           // gcc 4.8
+    // char _filler[56];           // gcc 4.9
 } PyDictObject;
 
 // Pyston change: these are no longer static objects:
--- from_cpython/Include/object.h
+++ from_cpython/Include/object.h
@@ -454,7 +454,9 @@ struct _typeobject {
 
     void* _hcls;
     void* _hcattrs;
-    char _dep_getattrs[56]; // FIXME: this is hardcoding the size of this particular implementation of std::unordered_map
+    // FIXME: this is hardcoding the size of this particular implementation of std::unordered_map
+    char _dep_getattrs[56];     // gcc 4.8
+    // char _dep_getattrs[64];     // gcc 4.9
     char _ics[32];
     void* _gcvisit_func;
     void* _dtor;
