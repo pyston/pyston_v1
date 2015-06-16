77b84b6ff85a38b0136fa2a58e52701919213d0d
Kevin Modzelewski <kevmod@gmail.com>
2015-06-16 20:38:18 +0000

    This test should be working now

--- test/tests/sre_compile_test.py
+++ test/tests/sre_compile_test.py
@@ -1,13 +1,18 @@
-# skip-if: True
-# This test works but 1) is very slow [the importing is, not the regex itself], and 2) throws warnings
-# This test also seems to leak a lot of memory.
-
 import sre_compile
+import sre_constants
+
 r = sre_compile.compile("a(b+)c", 0)
 print r.match("")
 print r.match("ac")
 print r.match("abc").groups()
 for i in xrange(100000):
     r.match("abbc").groups()
-    if i % 1000 == 0:
+    if i % 10000 == 0:
         print i
+
+def identity(o):
+    return o
+
+charset = [(sre_constants.RANGE, (128, 65535))]
+
+print sre_compile._optimize_charset(charset, identity)
