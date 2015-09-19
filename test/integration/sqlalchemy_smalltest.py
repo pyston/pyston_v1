19cc862cb88a3560840908c0bccecf0f2c67bfdf
Kevin Modzelewski <kmod@dropbox.com>
2015-09-19 00:17:23 +0000

    These sqlalchemy tests are also currently working

--- test/integration/sqlalchemy_smalltest.py
+++ test/integration/sqlalchemy_smalltest.py
@@ -65,6 +65,40 @@ sqlalchemy.testing.plugin.pytestplugin.pytest_sessionstart(None)
 
 import glob
 test_files = glob.glob(TEST_DIR + "/test*.py") + glob.glob(TEST_DIR + "/*/test*.py")
+test_files.sort()
+
+CPYTHON_PASSING = [
+    'test.aaa_profiling.test_compiler', 'test.aaa_profiling.test_memusage',
+    'test.aaa_profiling.test_orm', 'test.aaa_profiling.test_pool', 'test.aaa_profiling.test_resultset',
+
+    'test.base.test_dependency', 'test.base.test_events', 'test.base.test_except', 'test.base.test_inspect', 'test.base.test_utils',
+
+    'test.dialect.test_mxodbc', 'test.dialect.test_pyodbc', 'test.dialect.test_sqlite', 'test.dialect.test_sybase',
+
+    'test.engine.test_bind', 'test.engine.test_ddlevents', 'test.engine.test_logging',
+    'test.engine.test_parseconnect', 'test.engine.test_pool', 'test.engine.test_reconnect',
+
+    'test.ext.test_compiler', 'test.ext.test_extendedattr', 'test.ext.test_hybrid', 'test.ext.test_orderinglist',
+
+    'test.orm.test_association', 'test.orm.test_assorted_eager', 'test.orm.test_attributes', 'test.orm.test_backref_mutations',
+    'test.orm.test_bind', 'test.orm.test_bulk', 'test.orm.test_bundle', 'test.orm.test_collection',
+    'test.orm.test_compile', 'test.orm.test_composites', 'test.orm.test_cycles', 'test.orm.test_defaults',
+    'test.orm.test_default_strategies', 'test.orm.test_deferred', 'test.orm.test_deprecations', 'test.orm.test_descriptor',
+    'test.orm.test_dynamic', 'test.orm.test_eager_relations', 'test.orm.test_evaluator', 'test.orm.test_events',
+    'test.orm.test_expire', 'test.orm.test_hasparent', 'test.orm.test_immediate_load', 'test.orm.test_inspect',
+    'test.orm.test_joins', 'test.orm.test_lazy_relations', 'test.orm.test_load_on_fks', 'test.orm.test_lockmode',
+    'test.orm.test_manytomany', 'test.orm.test_merge', 'test.orm.test_naturalpks', 'test.orm.test_of_type',
+    'test.orm.test_onetoone', 'test.orm.test_options', 'test.orm.test_query', 'test.orm.test_relationships',
+    'test.orm.test_rel_fn', 'test.orm.test_scoping', 'test.orm.test_selectable', 'test.orm.test_session',
+    'test.orm.test_sync', 'test.orm.test_transaction', 'test.orm.test_unitofworkv2', 'test.orm.test_update_delete',
+    'test.orm.test_utils', 'test.orm.test_validators', 'test.orm.test_versioning',
+
+    'test.sql.test_case_statement', 'test.sql.test_compiler', 'test.sql.test_constraints', 'test.sql.test_cte',
+    'test.sql.test_ddlemit', 'test.sql.test_delete', 'test.sql.test_functions', 'test.sql.test_generative',
+    'test.sql.test_insert', 'test.sql.test_inspect', 'test.sql.test_join_rewriting', 'test.sql.test_labels',
+    'test.sql.test_operators', 'test.sql.test_query', 'test.sql.test_quote', 'test.sql.test_rowcount',
+    'test.sql.test_selectable', 'test.sql.test_text', 'test.sql.test_unicode',
+]
 
 # These are the ones that pass on CPython (ie that we've stubbed enough of their testing
 # infrastructure to run):
@@ -77,15 +111,51 @@ MODULES_TO_TEST = [
     'test.dialect.test_mxodbc',
     'test.dialect.test_pyodbc',
     'test.dialect.test_sybase',
+    'test.engine.test_bind',
+    'test.engine.test_ddlevents',
     'test.engine.test_parseconnect',
+    'test.engine.test_reconnect',
     'test.ext.test_compiler',
+    'test.orm.test_association',
+    'test.orm.test_assorted_eager',
+    'test.orm.test_backref_mutations',
+    'test.orm.test_bind',
+    'test.orm.test_bulk',
+    'test.orm.test_bundle',
+    'test.orm.test_compile',
+    'test.orm.test_cycles',
+    'test.orm.test_defaults',
+    'test.orm.test_default_strategies',
+    'test.orm.test_deferred',
+    'test.orm.test_deprecations',
     'test.orm.test_descriptor',
+    'test.orm.test_eager_relations',
+    'test.orm.test_evaluator',
+    'test.orm.test_expire',
     'test.orm.test_inspect',
+    'test.orm.test_load_on_fks',
+    'test.orm.test_lockmode',
+    'test.orm.test_onetoone',
+    'test.orm.test_options',
     'test.orm.test_query',
+    'test.orm.test_rel_fn',
+    'test.orm.test_scoping',
+    'test.orm.test_selectable',
+    'test.orm.test_sync',
+    'test.orm.test_validators',
+    'test.sql.test_case_statement',
+    'test.sql.test_constraints',
     'test.sql.test_cte',
     'test.sql.test_ddlemit',
+    'test.sql.test_delete',
+    'test.sql.test_functions',
+    'test.sql.test_insert',
     'test.sql.test_inspect',
+    'test.sql.test_join_rewriting',
+    'test.sql.test_labels',
     'test.sql.test_operators',
+    'test.sql.test_query',
+    'test.sql.test_rowcount',
 ]
 
 passed = []
