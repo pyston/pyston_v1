238196dc63e514a6ea8983c6b87aeb5bcea19429
Kevin Modzelewski <kmod@dropbox.com>
2015-10-01 21:23:50 +0000

    babel wanted to set __builtins__
    I'm not quite sure what they're trying to use it for, but it looks
    like it doesn't really do anything.  And we're no worse off with the
    patch+assert than simply ignoring the behavior.

--- test/extra/babel_test.py
+++ test/extra/babel_test.py
@@ -12,6 +12,9 @@ NOSETESTS_EXE = os.path.abspath(os.path.join(ENV_NAME, "bin", "nosetests"))
 packages = ["nose==1.3.7", "pytz==2015.4", "-e", "git+http://github.com/mitsuhiko/babel.git@1.3#egg=Babel"]
 create_virtenv(ENV_NAME, packages, force_create = True)
 
+PATCH_FILE = os.path.abspath(os.path.join(os.path.dirname(__file__), "babel.patch"))
+subprocess.check_call(["patch", "-p1", "--input=" + PATCH_FILE], cwd=BABEL_DIR)
+
 subprocess.check_call([PYTHON_EXE, "setup.py", "import_cldr"], cwd=BABEL_DIR)
 subprocess.check_call([PYTHON_EXE, "setup.py", "build"], cwd=BABEL_DIR)
 subprocess.check_call([PYTHON_EXE, "setup.py", "install"], cwd=BABEL_DIR)
