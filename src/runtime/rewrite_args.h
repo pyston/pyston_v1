476ba43ecca8502398b8ee4d1fb50087e81a3c7f
Marius Wachtler <undingen@gmail.com>
2015-10-05 10:31:27 +0100

    Add more Rewritable template args to functions            django_template3.py             2.7s (4)             2.7s (4)  -0.3%                  pyxl_bench.py             2.3s (4)             2.2s (4)  -4.1%      sqlalchemy_imperative2.py             2.7s (4)             2.6s (4)  -1.4%                        geomean                 2.5s                 2.5s  -2.0%

--- src/runtime/rewrite_args.h
+++ src/runtime/rewrite_args.h
@@ -279,6 +279,7 @@ struct CompareRewriteArgs {
 // The caller is responsible for guarding for paramspec, argspec, param_names, and defaults.
 // TODO Fix this function's signature.  should we pass back out through args?  the common case is that they
 // match anyway.  Or maybe it should call a callback function, which could save on the common case.
+template <Rewritable rewritable = REWRITABLE>
 void rearrangeArguments(ParamReceiveSpec paramspec, const ParamNames* param_names, const char* func_name,
                         Box** defaults, _CallRewriteArgsBase* rewrite_args, bool& rewrite_success, ArgPassSpec argspec,
                         Box*& arg1, Box*& arg2, Box*& arg3, Box** args, Box** oargs,
