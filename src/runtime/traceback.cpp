bee872415ef786a40e228bb13fd21fc9253600fa
Marius Wachtler <undingen@gmail.com>
2015-11-17 16:50:50 +0000

    fix after rebase

--- src/runtime/traceback.cpp
+++ src/runtime/traceback.cpp
@@ -130,7 +130,7 @@ void setupTraceback() {
     traceback_cls->giveAttr("tb_lineno", new (pyston_getset_cls) BoxedGetsetDescriptor(traceback_tb_lineno, NULL,
     NULL));
     */
-    traceback_cls->giveAttr("tb_next", new (pyston_getset_cls) BoxedGetsetDescriptor(traceback_tb_next, NULL, NULL));
+    // traceback_cls->giveAttr("tb_next", new (pyston_getset_cls) BoxedGetsetDescriptor(traceback_tb_next, NULL, NULL));
 
     traceback_cls->freeze();
 }
