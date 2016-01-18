681c967e20fd8999f0beefb1c7877ef499c8834f
Marius Wachtler <undingen@gmail.com>
2016-01-18 17:05:44 +0000

    Add formencode test

---
+++ test/extra/formencode_test.py
@@ -0,0 +1,17 @@
+import os, sys, subprocess
+sys.path.append(os.path.dirname(__file__) + "/../lib")
+
+from test_helper import create_virtenv, run_test
+
+ENV_NAME = "formencode_test_env_" + os.path.basename(sys.executable)
+SRC_DIR = os.path.abspath(os.path.join(ENV_NAME, "src"))
+PYTHON_EXE = os.path.abspath(os.path.join(ENV_NAME, "bin", "python"))
+NOSETESTS_EXE = os.path.abspath(os.path.join(ENV_NAME, "bin", "nosetests"))
+FORMENCODE_DIR = os.path.abspath(os.path.join(ENV_NAME, "src", "formencode"))
+
+packages = ["nose==1.3.7", "pycountry==1.6", "pyDNS==2.3.6"]
+packages += ["-e", "git+https://github.com/formencode/formencode.git@1.2.5#egg=formencode"]
+create_virtenv(ENV_NAME, packages, force_create = True)
+
+expected = [{'ran': 201}]
+run_test([NOSETESTS_EXE], cwd=FORMENCODE_DIR, expected=expected)
