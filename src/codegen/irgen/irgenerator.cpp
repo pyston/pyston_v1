4a8fc67fedf03a447b5d850a041779f209ad0b57
Kevin Modzelewski <kevmod@gmail.com>
2016-03-18 23:04:22 +0000

    Have to set the vreg here as well

--- src/codegen/irgen/irgenerator.cpp
+++ src/codegen/irgen/irgenerator.cpp
@@ -2627,6 +2627,7 @@ private:
                 llvm::Value* v;
                 if (phi_type == phi_type->getBoxType()) {
                     v = emitter.getNone()->getValue();
+                    _setVRegIfUserVisible(*it, [&]() { return v; }, NULL);
                 } else {
                     v = llvm::UndefValue::get(phi_type->llvmType());
                 }
