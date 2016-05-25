f4f3d39fdc8c2bf3f8891671e30122f4ec59820e
Marius Wachtler <undingen@gmail.com>
2016-05-25 17:39:52 +0100

    exec, input: if globals has no __builtins__ add it as a dictwrapper before we added a it as a module which made code fail which does something like __builtins__["unicode"]

--- test/tests/input.py
+++ test/tests/input.py
@@ -3,9 +3,12 @@ import sys
 
 success_tests = ["1234",            # just a number
                  "    123",         # whitespaces get trimmed
-                 "str(5) + \"6\""   # test for builtin function
+                 "str(5) + \"6\"",  # test for builtin function
+                 "hasattr(__builtins__, 'get')"
                 ]
-special_tests = ["str(10)"]
+special_tests = ["str(10)",
+                 "hasattr(__builtins__, 'get')"
+                 ]
 failure_tests = ["abcd"]
 
 orig_stdin = sys.stdin
@@ -15,9 +18,10 @@ sys.stdin = sio
 for _ in success_tests:
     print repr(input())
 
-# Special test: if the globals is empty, __builtin__ should be added to it
-# in the call to input().
-print repr(eval("input()", {}))
+for _ in special_tests:
+    # Special test: if the globals is empty, __builtin__ should be added to it
+    # in the call to input().
+    print repr(eval("input()", {}))
 
 try:
     print repr(input())
