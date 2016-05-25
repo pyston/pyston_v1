a11ffa7386dbb2256ee2246f6cec6433f952a25d
Kevin Modzelewski <kevmod@gmail.com>
2016-05-25 01:25:05 +0000

    Manually specify the output of methoddescr.py
    The behavior changed in CPython 2.7.4, and Travis-CI runs 2.7.3.

--- test/tests/methoddescr.py
+++ test/tests/methoddescr.py
@@ -1,2 +1,5 @@
+# This code was legal in CPython 2.7.3 but became illegal in 2.7.4
+# (there is a methoddescr.expected file for this test)
+
 for i in xrange(1000):
-    print float.__dict__['fromhex'](float, "f0.04a")
+    float.__dict__['fromhex'](float, "f0.04a")
