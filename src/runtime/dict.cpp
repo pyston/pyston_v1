23f45a1cd83438bad318b9af1235f7ba4d159a6e
sizeoftank <cwei.eye@gmail.com>
2016-06-21 22:38:28 +0800

    Revert to earlier commit.

--- src/runtime/dict.cpp
+++ src/runtime/dict.cpp
@@ -664,10 +664,6 @@ Box* dictEq(BoxedDict* self, Box* _rhs) {
         raiseExcHelper(TypeError, "descriptor '__eq__' requires a 'dict' object but received a '%s'",
                        getTypeName(self));
 
-    if (_rhs == Py_None) {
-        Py_RETURN_FALSE;
-    }
-
     if (_rhs->cls == attrwrapper_cls) {
         _rhs = attrwrapperToDict(_rhs);
     } else {
@@ -1031,49 +1027,26 @@ Finished:
     return res;
 }
 
-
 static PyObject* dict_richcompare(PyObject* v, PyObject* w, int op) noexcept {
     Box* res;
 
-    if (w == Py_None) {
-        if (op == Py_NE || op == Py_GT) {
-            Py_RETURN_TRUE;
-        } else {
-            Py_RETURN_FALSE;
-        }
-    }
-
     if (!PyDict_Check(v) || !PyDict_Check(w)) {
-        res = Py_NotImplemented;
-        return incref(res);
-    }
-
-    if (op == Py_EQ) {
-        return dictEq((BoxedDict*)v, (BoxedDict*)w);
+        res = incref(Py_NotImplemented);
+    } else if (op == Py_EQ) {
+        res = dictEq((BoxedDict*)v, (BoxedDict*)w);
     } else if (op == Py_NE) {
-        return dictNe((BoxedDict*)v, (BoxedDict*)w);
+        res = dictNe((BoxedDict*)v, (BoxedDict*)w);
     } else {
-        int cmp = dict_compare((BoxedDict*)v, (BoxedDict*)w);
-        switch (op) {
-            case Py_LT:
-                res = (cmp < 0) ? Py_True : Py_False;
-                break;
-            case Py_LE:
-                res = (cmp <= 0) ? Py_True : Py_False;
-                break;
-            case Py_GT:
-                res = (cmp > 0) ? Py_True : Py_False;
-                break;
-            case Py_GE:
-                res = (cmp >= 0) ? Py_True : Py_False;
-                break;
-            default:
-                RELEASE_ASSERT(0, "%d", op);
+        /* Py3K warning if comparison isn't == or !=  */
+        if (PyErr_WarnPy3k("dict inequality comparisons not supported "
+                           "in 3.x",
+                           1) < 0) {
+            return NULL;
         }
-        return incref(res);
+        res = incref(Py_NotImplemented);
     }
+    return res;
 }
-
 
 void BoxedDict::dealloc(Box* b) noexcept {
     if (_PyObject_GC_IS_TRACKED(b))
@@ -1165,6 +1138,7 @@ void setupDict() {
         = dictiteritem_cls->instances_are_nonzero = true;
 
     dict_cls->tp_hash = PyObject_HashNotImplemented;
+    dict_cls->tp_compare = (cmpfunc)dict_compare;
     dict_cls->tp_richcompare = dict_richcompare;
 
     dict_cls->giveAttr("__len__", new BoxedFunction(FunctionMetadata::create((void*)dictLen, BOXED_INT, 1)));
