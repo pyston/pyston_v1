57ba9caaf1b2445b676e0a7b4e80ad85feb9dee6
Kevin Modzelewski <kevmod@gmail.com>
2016-07-02 00:13:17 +0000

    large_import_bench.py

---
+++ microbenchmarks/large_import_bench.py
@@ -0,0 +1,16 @@
+import re
+import os
+import sre_compile
+import weakref
+import threading
+import abc
+import _abcoll
+import collections
+import multiprocessing
+import argparse
+import bisect
+import heapq
+import inspect
+import netrc
+import pickle
+import socket
