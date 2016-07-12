b00a768e231d1f93b072ae78027f7614ca47067c
Kevin Modzelewski <kevmod@gmail.com>
2016-07-12 00:14:00 +0000

    Fix unexpected-AttributeError-when-importing
    The repro is:
    - import a.b, and in a.b do
    - import a.b as ab
    
    This will cause an attribute error, since a.b (the name) is successfully importable,
    but a.b (the attribute) doesn't exist yet.

--- test/tests/test_package/import_target.py
+++ test/tests/test_package/import_target.py
@@ -1,1 +1,27 @@
 print "running test_package.import_target"
+
+# Since we are currently importing test_package.import_target, this
+# import will succeed (return directly from sys.modules), even though
+# test_package will not have the 'import_target' attribute yet
+
+import test_package.import_target
+
+try:
+    print test_package.import_target
+    assert 0
+except AttributeError:
+    pass
+
+try:
+    print getattr(test_package, 'import_target')
+    assert 0
+except AttributeError:
+    pass
+
+# You can do 'import test_package.import_target', but adding an asname will cause an exception:
+try:
+    import test_package.import_target as i
+    assert 0
+    i
+except AttributeError:
+    pass
