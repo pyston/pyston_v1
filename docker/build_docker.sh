b4b69eb926bfc20048c426518ac7054c7bf7abaf
Kevin Modzelewski <kevmod@gmail.com>
2016-07-15 00:16:23 +0000

    fix duplicate docker builds

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -5,18 +5,14 @@ set -u
 BUILD_NAME=0.5.1
 
 for type in pyston pyston-numpy; do
-    for tag in latest ${BUILD_NAME}; do
-        cd ~/pyston/docker/${type}
-        # "docker build" only recently gained the ability to take multiple -t flags
-        docker build -t pyston/${type}:latest ~/pyston/docker/${type}
-        docker build -t pyston/${type}:${BUILD_NAME} ~/pyston/docker/${type}
-    done
+    cd ~/pyston/docker/${type}
+    # "docker build" only recently gained the ability to take multiple -t flags
+    docker build -t pyston/${type}:latest ~/pyston/docker/${type}
+    docker build -t pyston/${type}:${BUILD_NAME} ~/pyston/docker/${type}
 done
 
 for type in pyston pyston-numpy; do
-    for tag in latest ${BUILD_NAME}; do
-        cd ~/pyston/docker/${type}
-        docker push pyston/${type}:latest
-        docker push pyston/${type}:${BUILD_NAME}
-    done
+    cd ~/pyston/docker/${type}
+    docker push pyston/${type}:latest
+    docker push pyston/${type}:${BUILD_NAME}
 done
