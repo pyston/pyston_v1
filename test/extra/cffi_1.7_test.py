7f5b72b86b8ec25973ed4ee59e8375ab631988f0
Kevin Modzelewski <kevmod@gmail.com>
2016-08-20 00:29:07 +0000

    Add cffi v1.7 test
    This new version is causing some issues for us

--- test/extra/cffi_1.7_test.py
+++ test/extra/cffi_1.7_test.py
@@ -4,7 +4,7 @@ sys.path.append(os.path.dirname(__file__) + "/../lib")
 
 from test_helper import create_virtenv, run_test
 
-ENV_NAME = "cffi_test_env_" + os.path.basename(sys.executable)
+ENV_NAME = "cffi17_test_env_" + os.path.basename(sys.executable)
 SRC_DIR = os.path.abspath(os.path.join(ENV_NAME, "src"))
 PYTHON_EXE = os.path.abspath(os.path.join(ENV_NAME, "bin", "python"))
 PYTEST_EXE = os.path.abspath(os.path.join(ENV_NAME, "bin", "py.test"))
@@ -13,13 +13,10 @@ def install_and_test_cffi():
     shutil.rmtree(SRC_DIR, ignore_errors=True)
     os.makedirs(SRC_DIR)
 
-    url = "https://pypi.python.org/packages/source/c/cffi/cffi-1.2.1.tar.gz"
+    url = "https://pypi.python.org/packages/83/3c/00b553fd05ae32f27b3637f705c413c4ce71290aa9b4c4764df694e906d9/cffi-1.7.0.tar.gz#md5=34122a545060cee58bab88feab57006d"
     subprocess.check_call(["wget", url], cwd=SRC_DIR)
-    subprocess.check_call(["tar", "-zxf", "cffi-1.2.1.tar.gz"], cwd=SRC_DIR)
-    CFFI_DIR = os.path.abspath(os.path.join(SRC_DIR, "cffi-1.2.1"))
-
-    PATCH_FILE = os.path.abspath(os.path.join(os.path.dirname(__file__), "cffi-1.2.1.patch"))
-    subprocess.check_call(["patch", "-p1", "--input=" + PATCH_FILE], cwd=CFFI_DIR)
+    subprocess.check_call(["tar", "-zxf", "cffi-1.7.0.tar.gz"], cwd=SRC_DIR)
+    CFFI_DIR = os.path.abspath(os.path.join(SRC_DIR, "cffi-1.7.0"))
 
     subprocess.check_call([PYTHON_EXE, "setup.py", "install"], cwd=CFFI_DIR)
 
