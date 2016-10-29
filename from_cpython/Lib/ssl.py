5d79d3648a27dc7c8918b7dded019ff9f1064e36
Feram <feram@feram.co>
2016-10-29 15:18:00 +0000

    Fix typo compability

--- from_cpython/Lib/ssl.py
+++ from_cpython/Lib/ssl.py
@@ -464,7 +464,7 @@ def get_protocol_name(protocol_code):
 def sslwrap_simple(sock, keyfile=None, certfile=None):
 
     """A replacement for the old socket.ssl function.  Designed
-    for compability with Python 2.5 and earlier.  Will disappear in
+    for compatibility with Python 2.5 and earlier.  Will disappear in
     Python 3.0."""
 
     if hasattr(sock, "_sock"):
