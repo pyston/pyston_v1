026c98845e1e494da3eafd45f698058531e0c2ad
Boxiang Sun <daetalusun@gmail.com>
2016-11-22 18:07:55 +0800

    Add PyObject_GetDict and other two APIs
    Pyston can not support _PyObject_GetDictPtr properly. So add
    PyObject_GetDictCopy, PyObject_ClearDict, PyObject_UpdateDict API.
    
    The PyObject_GetDictCopy only return an copy of object's dict, not a
    writable dict pointer like _PyObject_GetDictPtr.
    
    The PyObject_UpdateDict only update or append new key-value pairs from
    new dict. It will not reset object's dict if the key is not existed
    in the new dict.

--- test/test_extension/api_test.c
+++ test/test_extension/api_test.c
@@ -32,11 +32,38 @@ change_self(PyObject* self, PyObject* args) {
     Py_RETURN_NONE;
 }
 
+static PyObject*
+dict_API_test(PyObject* self, PyObject* args) {
+    PyObject *inst;
+    if (!PyArg_ParseTuple(args, "O", &inst))
+        return NULL;
+
+#if defined(PYSTON_VERSION)
+    // Get `name` field in inst.
+    PyObject* dict = PyObject_GetDictCopy(inst);
+    if (dict == Py_None)
+        return NULL;
+    PyObject* name = PyDict_GetItem(dict, PyString_FromString("name"));
+
+    // Clear the object's dict.
+    PyObject_ClearDict(inst);
+    PyObject* old_dict = PyObject_GetDictCopy(inst);
+    // Set new items for object's dict.
+    PyObject* new_dict = PyDict_New();
+    PyDict_SetItem(new_dict, PyString_FromString("value"), PyInt_FromLong(42));
+    PyObject_UpdateDict(inst, new_dict);
+
+    // The return values should be 'Pyston', {}
+    return Py_BuildValue("OO", name, old_dict);
+#endif
+    return Py_BuildValue("OO", Py_None, Py_None);
+}
+
 static PyMethodDef TestMethods[] = {
     {"set_size",  set_size, METH_O, "Get set size by PySet_Size." },
     {"test_attrwrapper_parse",  test_attrwrapper_parse, METH_VARARGS, "Test PyArg_ParseTuple for attrwrappers." },
-    {"change_self",  change_self, METH_VARARGS,
-     "A function which the self point to its base class."},
+    {"change_self",  change_self, METH_VARARGS, "A function which the self point to its base class."},
+    {"dict_API_test",  dict_API_test, METH_VARARGS, ""},
     {NULL, NULL, 0, NULL}        /* Sentinel */
 };
 
--- test/tests/capi_test.py
+++ test/tests/capi_test.py
@@ -16,3 +16,19 @@ foo = b.foo
 foo()
 api_test.change_self(foo, a)
 foo()
+
+try:
+    import __pyston__
+    class A(object):
+        def __init__(self):
+            self.name = 'Pyston'
+    a = A()
+    # this C function will test three things:
+    # - Use PyObject_GetDict to get object's dict and return the obj.name
+    # - Use PyObjcet_ClearDict to clear object's dict.
+    # - Use PyObject_SetDict to add {'value': 42} to object's dict.
+    ret, stub = api_test.dict_API_test(a)
+    assert(ret == 'Pyston')
+    assert(a.__dict__['value'] == 42)
+except ImportError:
+    pass
